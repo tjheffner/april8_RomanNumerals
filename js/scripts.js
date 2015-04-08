573a2bffbbd2e58ef75236c4b53f1f04a06d4bae
Bryan Borgeson & Tanner Heffner <student@epicodus.com>
2015-04-08 14:04:04 -0700

    add test & logic to return IV

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 var romanNumeral = function(value) {
 
+    var allRomanNumerals = {'I': 1, 'V': 5};
+    var valueRoman = [];
 
+    for (var i = 1; i <= value; i++) {
+        valueRoman.push('I');
+
+    if (valueRoman.join("") === "IIII") {
+        valueRoman.splice(1, valueRoman.length, "V");
+    }
+
+    }
+    return valueRoman.join("");
+
+    // 4 => if greater than 3, then take the next roman numeral value and subtract the value of the previous roman numeral from it,
+    // 5 - 1 = 4
 
 };
 
--- specs/specs.js
+++ specs/specs.js
@@ -1,7 +1,17 @@
-describe("", function() {
-    it("", function() {
-
+describe("romanNumeral", function() {
+    it("should return 'I' for an input value of 1", function() {
+        expect(romanNumeral(1)).to.equal('I');
     });
 
+    it("should return 'II' for an input value of 2", function() {
+        expect(romanNumeral(2)).to.equal('II');
+    });
 
+    it("should return 'III' for an input value of 3", function() {
+        expect(romanNumeral(3)).to.equal('III');
+    });
+
+    it("should return 'IV' for an input value of 4", function() {
+        expect(romanNumeral(4)).to.equal('IV');
+    });
 });
