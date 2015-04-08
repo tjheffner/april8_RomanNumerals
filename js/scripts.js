4144daa01b1651ca090d9194f601cc764ed344c5
Bryan Borgeson & Tanner Heffner <student@epicodus.com>
2015-04-08 15:53:16 -0700

    fix logic for IIII to IV and add test/logic for 90

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,67 @@
 var romanNumeral = function(value) {
 
-    var allRomanNumerals = {'I': 1, 'V': 5};
+    // var allRomanNumerals = {'I': 1, 'V': 5, 'X': 10};
     var valueRoman = [];
 
-    for (var i = 1; i <= value; i++) {
-        valueRoman.push('I');
+    var four = "IIII";
 
-    if (valueRoman.join("") === "IIII") {
-        valueRoman.splice(1, valueRoman.length, "V");
+    while (value > 0) {
+        switch (value > 0) {
+
+            case ((value >= 90) && (value < 99)):
+                valueRoman.push('XC');
+                value = value - 90;
+                break;
+
+            case (value >= 50):
+                valueRoman.push('L');
+                value = value - 50;
+                break;
+
+            case ((value >= 40) && (value < 49)):
+                valueRoman.push('XL');
+                value = value - 40;
+                break;
+
+            case (value >= 10):
+                valueRoman.push('X');
+                value = value - 10;
+                break;
+
+            case (value === 9):
+                valueRoman.push('IX');
+                value = value - 9;
+                break;
+
+            case ((value >= 5) && (value < 9)):
+                valueRoman.push('V');
+                value = value - 5;
+                break;
+
+            case ((value < 5) && (value >= 0)):
+                for (var i = 1; i <= value; i++) {
+                    valueRoman.push('I');
+                    value = value - 1;
+
+                    var convert_four = valueRoman.filter(function(element, index, array) {
+                        return four.split("").indexOf(element) != -1;
+                    });
+
+                    var test = convert_four.join("");
+
+                    if (test === "IIII") {
+                        valueRoman.splice((valueRoman.join("").length - 3), valueRoman.length, "V");
+                    }
+                }
+                break;
+
+            //this only runs if nothing else does. stops our while loop.
+            default: value = 0;
+            break;
+        }
     }
 
-    }
     return valueRoman.join("");
-
-    // 4 => if greater than 3, then take the next roman numeral value and subtract the value of the previous roman numeral from it,
-    // 5 - 1 = 4
 
 };
 
--- specs/specs.js
+++ specs/specs.js
@@ -14,4 +14,52 @@ describe("romanNumeral", function() {
     it("should return 'IV' for an input value of 4", function() {
         expect(romanNumeral(4)).to.equal('IV');
     });
+
+    it("should return 'V' for an input value of 5", function() {
+        expect(romanNumeral(5)).to.equal('V');
+    });
+
+    it("should return 'VI' for an input value of 6", function() {
+        expect(romanNumeral(6)).to.equal('VI');
+    });
+
+    it("should return 'VII' for an input value of 7", function() {
+        expect(romanNumeral(7)).to.equal('VII');
+    });
+
+    it("should return 'IX' for an input value of 9", function() {
+        expect(romanNumeral(9)).to.equal('IX');
+    });
+
+    it("should return 'X' for an input value of 10", function() {
+        expect(romanNumeral(10)).to.equal('X');
+    });
+
+    it("should return 'XI' for an input value of 11", function() {
+        expect(romanNumeral(11)).to.equal('XI');
+    });
+
+    it("should return 'XXI' for an input value of 21", function() {
+        expect(romanNumeral(21)).to.equal('XXI');
+    });
+
+    it("should return 'XXVIII' for an input value of 28", function() {
+        expect(romanNumeral(28)).to.equal('XXVIII');
+    });
+
+    it("should return 'XL' for an input value of 40", function() {
+        expect(romanNumeral(40)).to.equal('XL');
+    });
+
+    it("should return 'L' for an input value of 50", function() {
+        expect(romanNumeral(50)).to.equal('L');
+    });
+
+    it("should return 'LIV' for an input value of 54", function() {
+        expect(romanNumeral(54)).to.equal('LIV');
+    });
+
+    it("should return 'XC' for an input value of 90", function() {
+        expect(romanNumeral(90)).to.equal('XC');
+    });
 });
